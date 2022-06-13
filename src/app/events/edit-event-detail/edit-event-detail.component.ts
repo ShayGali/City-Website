d305274c00fd5def5ba941707f69a461c4c6c199
ShayG0 <86621168+ShayG0@users.noreply.github.com>
2022-06-13 15:52:12 +0300

    start edit event detail

--- src/app/events/edit-event-detail/edit-event-detail.component.ts
+++ src/app/events/edit-event-detail/edit-event-detail.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { EventService } from '../event.service';
+import EventData from '../EventData';
 
 @Component({
   selector: 'app-edit-event-detail',
   templateUrl: './edit-event-detail.component.html',
-  styleUrls: ['./edit-event-detail.component.css']
+  styleUrls: ['./edit-event-detail.component.css'],
 })
 export class EditEventDetailComponent implements OnInit {
-
-  constructor() { }
+  event: Observable<EventData>;
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private service: EventService
+  ) {}
 
   ngOnInit(): void {
+    this.event = this.route.paramMap.pipe(
+      switchMap((params: ParamMap) =>
+        this.service.getEventByCode(+params.get('id'))
+      )
+    );
   }
 
+  goBack() {
+    this.router.navigate(['/events']);
+  }
 }
